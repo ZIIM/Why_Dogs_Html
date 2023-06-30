7bee4c9db843eabbab927390a9c40f3396ef9210
ZIIM <johntgause12@gmail.com>
2023-06-30 02:51:37 -0500

    added prompts and photos to accomidate answer

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-function sayFavoriteAnimal(greeting){
-    let favorite = "Dogs";
-
-    console.log(greeting + favorite);
-
-    return greeting;
+function dogoPeople() {
+    let likesDogs = confirm("Do you like Dogs?");
+    return likesDogs;
 }
+  
+function forDogoPeople() {
+    const defaultAnswer = "";
+  
+    let response = prompt('What is your favorite dog breed?', defaultAnswer);
+  
+    while (response.length == 0) {
+      response = prompt('Enter for your favorite dog breed. Please and Thank you.', defaultAnswer);
+    }
+  
+    document.write('You like dogs, and your favorite dog breed is ' + response + '.');
+  
+}
+
+
+
